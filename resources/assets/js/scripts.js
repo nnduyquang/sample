e13dd072c9666c9ad3f9cddc8365fd9d97a6b854
QUANG_CTY <nnduyquang@gmail.com>
2017-03-22 15:24:45 +0700

    #Done Carousel

--- resources/assets/js/scripts.js
+++ resources/assets/js/scripts.js
@@ -1,24 +1,27 @@
 var plugins = {
-    owlCarousel: $('.owl-carousel')
+    owlCarousel: $("#slider")
 };
 $(document).ready(function () {
+
     function loadOwlCarousel() {
-        plugins.owlCarousel.owlCarousel({
-            navigation: true,
-            slideSpeed: 500,
-            paginationSpeed: 500,
-            navText: ["<i class='fa fa-angle-left'></i>", "<i class='fa fa-angle-right'></i>"],
+        var owl = plugins.owlCarousel.owlCarousel({
             items: 1,
+            // slideSpeed : 2000,
             animateIn: 'fadeIn',
             animateOut: 'fadeOut',
             nav: true,
-            dots:true
-
-        });
+            autoplay: true,
+            dots: true,
+            loop: true,
+            responsiveRefreshRate: 200,
+            navText: ["<i class='fa fa-angle-left'></i>", "<i class='fa fa-angle-right'></i>"],
+        })
+        owl.trigger('refresh.owl.carousel');
     }
 
     if (plugins.owlCarousel.length) {
         loadOwlCarousel()
     }
+
 
 });
--- resources/views/slider/owlcarousel.blade.php
+++ resources/views/slider/owlcarousel.blade.php
@@ -1,5 +1,8 @@
-<div id="owl-slide" class="owl-carousel owl-theme">
-    <div class="item">{{ Html::image('images/slider/slider_01.jpg','',array('class'=>'no-style')) }}</div>
-    <div class="item">{{ Html::image('images/slider/slider_02.jpg','',array('class'=>'no-style')) }}</div>
-    <div class="item">{{ Html::image('images/slider/slider_03.jpg','',array('class'=>'no-style')) }}</div>
+<div id="slider" class="owl-carousel owl-theme">
+    <div class="item">
+        {{ Html::image('images/slider/slider_01.jpg','',array('class'=>'no-style')) }}</div>
+    <div class="item">
+        {{ Html::image('images/slider/slider_02.jpg','',array('class'=>'no-style')) }}</div>
+    <div class="item">
+        {{ Html::image('images/slider/slider_03.jpg','',array('class'=>'no-style')) }}</div>
 </div>
