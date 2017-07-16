dfb82f23531bee57da6ab86350069f5050676c75
Quang-PC <nnduyquang@gmail.com>
2017-07-16 19:53:46 +0700

    ok

--- resources/views/master.blade.php
+++ resources/views/master.blade.php
@@ -19,36 +19,38 @@
 
 </header>
 <div id="blurrMe">
-    <div class="container hidden-lg hidden-md">
-        <div style="background-color: #1ba8ff" class="mobile header fixed-header navbar-fixed-top " role="navigation">
-            <ul class="nav nav-pills pull-left">
-                <li><a href="#" id="trigger"><i class="fa fa-bars" aria-hidden="true"></i></a></li>
-                <li style="padding: 10px 15px;color: white;font-weight: bolder"> @yield('title')</li>
-            </ul>
-            <div class="col-md-12 col-xs-12">
-                <form id="searchForm" main-title="" search-input=""><input
-                            placeholder="Tìm sản phẩm, danh mục hay thương hiệu..." autocomplete="off" name="q"
-                            type="search" class="form-control" id="searchInput" is="iron-input"></form>
-            </div>
-        </div>
-    </div>
-    //menu desktop
+    {{--<div class="container hidden-lg hidden-md">--}}
+        {{--<div style="background-color: #1ba8ff" class="mobile header fixed-header navbar-fixed-top " role="navigation">--}}
+            {{--<ul class="nav nav-pills pull-left">--}}
+                {{--<li><a href="#" id="trigger"><i class="fa fa-bars" aria-hidden="true"></i></a></li>--}}
+                {{--<li style="padding: 10px 15px;color: white;font-weight: bolder"> @yield('title')</li>--}}
+            {{--</ul>--}}
+            {{--<div class="col-md-12 col-xs-12">--}}
+                {{--<form id="searchForm" main-title="" search-input=""><input--}}
+                            {{--placeholder="Tìm sản phẩm, danh mục hay thương hiệu..." autocomplete="off" name="q"--}}
+                            {{--type="search" class="form-control" id="searchInput" is="iron-input"></form>--}}
+            {{--</div>--}}
+        {{--</div>--}}
+    {{--</div>--}}
+    @include('m-menu')
+    @include('menu1')
     <div id="wrap-container">
         @yield('container')
     </div>
 </div>
-<div class="sidebar closed">
-    <header>
-        <div class="background-sidebar"><h2><a href="#" id="close"> <i class="fa fa-arrow-left" aria-hidden="true"></i></a></h2></div>
-    </header>
-    <ul>
-        <li><a href="{{URL::to('/')}}"><i class="fa fa-home icon-sidebar" aria-hidden="true"></i>Trang Chủ</a></li>
-        <li><a href="{{URL::to('category')}}"><i class="fa fa-list icon-sidebar" aria-hidden="true"></i>Danh Sách Ngành Hàng</a></li>
-        <li><a href="#"><i class="fa fa-user-circle icon-sidebar" aria-hidden="true"></i>Quản Lý Tài Khoản</a></li>
-    </ul>
-    <div class="sidebar-sub-heading">HỖ TRỢ</div>
-    <div class="sidebar-hotline">HOTLINE: <span class="sidebar-number">0907468264</span></div>
-</div>
+@include('m-sidebar')
+{{--<div class="sidebar closed">--}}
+    {{--<header>--}}
+        {{--<div class="background-sidebar"><h2><a href="#" id="close"> <i class="fa fa-arrow-left" aria-hidden="true"></i></a></h2></div>--}}
+    {{--</header>--}}
+    {{--<ul>--}}
+        {{--<li><a href="{{URL::to('/')}}"><i class="fa fa-home icon-sidebar" aria-hidden="true"></i>Trang Chủ</a></li>--}}
+        {{--<li><a href="{{URL::to('category')}}"><i class="fa fa-list icon-sidebar" aria-hidden="true"></i>Danh Sách Ngành Hàng</a></li>--}}
+        {{--<li><a href="#"><i class="fa fa-user-circle icon-sidebar" aria-hidden="true"></i>Quản Lý Tài Khoản</a></li>--}}
+    {{--</ul>--}}
+    {{--<div class="sidebar-sub-heading">HỖ TRỢ</div>--}}
+    {{--<div class="sidebar-hotline">HOTLINE: <span class="sidebar-number">0907468264</span></div>--}}
+{{--</div>--}}
 
 <div class="footer">
     {{--@include('frontend.footer.footer')--}}
---
+++ resources/views/menu1.blade.php
@@ -0,0 +1,95 @@
+<div class="menu-header hidden-xs hidden-sm">
+    <div class="container">
+        <div class="clearfix">
+            <div class="menu-logo">
+                <a href="/">
+                    {{ Html::image('images/test/logo.svg','',array('styles'=>'width:138px')) }}
+                </a>
+            </div>
+            <div class="menu-header-info" style="margin-top: 0px">
+                <div class="clearfix">
+                    <div class="pull-right"></div>
+                    <div class="pull-right"></div>
+                    <div class="pull-right">
+                        <div class="menu-header-info-phone">
+                            <i class="fa fa-phone icon-phone" aria-hidden="true"></i>
+                            <div class="info-phone">
+                                <span class="phone-label">Hotline:</span>
+                                <span class="phone-heading-font"><a href="tel:0907468264">0907468264</a></span>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="pull-right">
+                        <div class="menu-header-info-address">
+                            <i class="fa fa-map-marker icon-address" aria-hidden="true"></i>
+                            <div class="info-address">
+                                <span class="address-heading-font">1840 E Garvey Ave South West Covina, CA 91791</span>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+<div class="menu-main hidden-xs hidden-sm">
+    <div class="container">
+        <div class="menu-left pull-left">
+            <ul>
+                <li class="current-menu-item"><a href="#">Home</a></li>
+                <li class="has-item-down"><a href="#" >Menu 1</a>
+                    <ul>
+                        <li><a href="#">Sub Menu 1</a></li>
+                        <li><a href="#">Sub Menu 2</a></li>
+                        <li><a href="#">Sub Menu 3</a></li>
+                        <li class="has-item-right"><a href="#">Sub Menu 4</a>
+                            <ul>
+                                <li class="has-item-right"><a href="#">Deep Menu 1</a>
+                                    <ul>
+                                        <li><a href="#">Sub Deep 1</a></li>
+                                        <li><a href="#">Sub Deep 2</a></li>
+                                        <li><a href="#">Sub Deep 3</a></li>
+                                        <li><a href="#">Sub Deep 4</a></li>
+                                    </ul>
+                                </li>
+                                <li><a href="#">Deep Menu 2</a></li>
+                            </ul>
+                        </li>
+                        <li><a href="#">Sub Menu 5</a></li>
+                    </ul>
+                </li>
+                <li class="has-item-down"><a href="#">Menu 2</a>
+                    <ul>
+                        <li><a href="#">Sub Menu 1</a></li>
+                        <li><a href="#">Sub Menu 2</a></li>
+                        <li><a href="#">Sub Menu 3</a></li>
+                    </ul>
+                </li>
+                <li class="has-item-down"><a href="#">Menu 3</a>
+                    <ul>
+                        <li class="dir"><a href="#">Sub Menu 1</a></li>
+                        <li class="dir has-item-right">
+                            <a href="#">Sub Menu 2</a>
+                            <ul>
+                                <li><a href="#">Category 1</a></li>
+                                <li><a href="#">Category 2</a></li>
+                                <li><a href="#">Category 3</a></li>
+                                <li><a href="#">Category 4</a></li>
+                                <li><a href="#">Category 5</a></li>
+                            </ul>
+                        </li>
+                        <li><a href="#">Sub Menu 3</a></li>
+                        <li><a href="#">Sub Menu 4</a></li>
+                        <li><a href="#">Sub Menu 5</a></li>
+                    </ul>
+                </li>
+                <li><a href="#">Menu 4</a></li>
+                <li><a href="#">Menu 5</a></li>
+                <li><a href="#">Menu 6</a></li>
+                <li><a href="#">Contact Us</a></li>
+            </ul>
+        </div>
+        <div class="menu-right"></div>
+    </div>
+
+</div>
